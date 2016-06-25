40123aa652b8993d549038d0a06cd0990d6c7ec7
Omar David Garcia <Omar David Garcia>
2016-06-25 04:53:26 -0400

    styles and new functions for the counter component

--- app/components/counter.jsx
+++ app/components/counter.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 /* STYLES  IN JSX*/
-var ButtonStyles = {
+var ButtonIncrement = {
     background: '#f1c40f',
     border: 0,
     boxShadow: '0px 5px 0px #927608',
@@ -11,6 +11,34 @@ var ButtonStyles = {
     outline: 'none',
     borderRadius: '3px',
     color: '#703688',
+    fontWeight: 'bold',
+    cursor: 'pointer'
+}
+
+var ButtonDecrement = {
+    marginTop: '20px',
+    background: '#CDCDCD',
+    border: 0,
+    boxShadow: '0px 5px 0px #9E9E9E',
+    padding: '20px',
+    width: '100%',
+    outline: 'none',
+    borderRadius: '3px',
+    color: '#703688',
+    fontWeight: 'bold',
+    cursor: 'pointer'
+}
+
+var restartStyle = {
+    marginTop: '20px',
+    background: 'white',
+    border: 0,
+    boxShadow: '0px 5px 0px #b7b7b7',
+    padding: '20px',
+    width: '100%',
+    outline: 'none',
+    borderRadius: '3px',
+    color: 'black',
     fontWeight: 'bold',
     cursor: 'pointer'
 }
@@ -30,6 +58,16 @@ var Counter = React.createClass({
       count: this.state.count + 1
     });
   },
+  decrementCount: function() {
+      this.setState({
+          count: this.state.count -1
+      });
+  },
+  restartCount: function(){
+      this.setState({
+          count: this.state.count = 0
+      });
+  },
   getInitialState: function(){
      return {
        count: 0
@@ -39,7 +77,9 @@ var Counter = React.createClass({
     return (
       <div class="my-component">
         <h1 style = {Header1Style}>Count: {this.state.count}</h1>
-        <button style = {ButtonStyles} type="button" onClick={this.incrementCount}>Increment</button>
+        <button style = {ButtonIncrement} type="button" onClick={this.incrementCount}>Increment</button>
+        <button style = {ButtonDecrement} type="button" onClick={this.decrementCount}>Decrement</button>
+        <button style = {restartStyle} type="button" onClick={this.restartCount}>Restart</button>
       </div>
     );
   }
